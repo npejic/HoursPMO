2ab82a4f5f7a2cbb073f620d9dd67fee5122e6a3
nik <npejic@gmail.com>
2016-10-01 23:21:18 +0200

    BaseClass created

---
+++ HoursPMO/Models/BaseClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoursPMO.Models
+{
+    /// <summary>
+    /// Class from which will all classes in Model inherit creation date
+    /// </summary>
+    public class BaseClass
+    {
+        public BaseClass()
+        {
+            this.Created = DateTime.Now;
+        }
+        public DateTime Created { get; set; }
+    }
+}
