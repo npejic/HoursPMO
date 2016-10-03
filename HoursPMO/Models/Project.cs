d9fa6d09dcef9d20caf00d8b3a18e5d615910c13
nik <npejic@gmail.com>
2016-10-03 22:35:31 +0200

    Added Project class

---
+++ HoursPMO/Models/Project.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HoursPMO.Models
+{
+    /// <summary>
+    /// Class that holds Project properties and connections to User and Client classes
+    /// </summary>
+    public class Project : BaseClass
+    {
+        /// <summary>
+        /// ID for Project
+        /// </summary>
+        public int ProjectID { get; set; }
+        /// <summary>
+        /// Property for Project name, type of string
+        /// </summary>
+        public string ProjectName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [ForeignKey("ResponsiblePerson")]
+        public int ResponsiblePersonID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual User ResponsiblePerson { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ClientID { get; set; }
+        /// <summary>
+        /// Navigation property
+        /// </summary>
+        public virtual Client Client { get; set; }
+
+        /// <summary>
+        /// Navigation property
+        /// </summary>
+        public virtual ICollection<WeekPerUserPerProject> WeekPerUserPerProjects { get; set; }
+    }
+}
