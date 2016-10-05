1f595a97f1900459b762f1e0da65c7442bc8df0e
nik <npejic@gmail.com>
2016-10-05 08:52:53 +0200

    Added ProjectGroup class to ViewModel folder

---
+++ HoursPMO/ViewModels/ProjectGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HoursPMO.Models;
+
+namespace HoursPMO.ViewModels
+{
+    public class ProjectGroup
+    {
+        public int ProjectID { get; set; }
+        public int FTOCount { get; set; }
+        public int LeavesCount { get; set; }
+        public int PossibleCount { get; set; }
+        public int ActualCount { get; set; }
+        public int BillableCount { get; set; }
+        public int WorkingDaysCount { get; set; }
+
+        public virtual Project Project { get; set; }
+    }
+}
