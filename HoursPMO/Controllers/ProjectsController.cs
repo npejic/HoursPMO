90c1c36884e218464afd659eeac7c512337240ac
nik <npejic@gmail.com>
2016-10-05 08:36:50 +0200

    Added Controlers and Views

---
+++ HoursPMO/Controllers/ProjectsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoursPMO.DAL;
+using HoursPMO.Models;
+
+namespace HoursPMO.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private HoursContext db = new HoursContext();
+
+        // GET: Projects
+        public ActionResult Index()
+        {
+            var projects = db.Projects.Include(p => p.Client).Include(p => p.ResponsiblePerson);
+            return View(projects.ToList());
+        }
+
+        // GET: Projects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientID = new SelectList(db.Clients, "ClientID", "ClientName");
+            ViewBag.ResponsiblePersonID = new SelectList(db.Users, "UserID", "UserName");
+            return View();
+        }
+
+        // POST: Projects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProjectID,ProjectName,ResponsiblePersonID,ClientID,Created")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClientID = new SelectList(db.Clients, "ClientID", "ClientName", project.ClientID);
+            ViewBag.ResponsiblePersonID = new SelectList(db.Users, "UserID", "UserName", project.ResponsiblePersonID);
+            return View(project);
+        }
+
+        // GET: Projects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientID = new SelectList(db.Clients, "ClientID", "ClientName", project.ClientID);
+            ViewBag.ResponsiblePersonID = new SelectList(db.Users, "UserID", "UserName", project.ResponsiblePersonID);
+            return View(project);
+        }
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProjectID,ProjectName,ResponsiblePersonID,ClientID,Created")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientID = new SelectList(db.Clients, "ClientID", "ClientName", project.ClientID);
+            ViewBag.ResponsiblePersonID = new SelectList(db.Users, "UserID", "UserName", project.ResponsiblePersonID);
+            return View(project);
+        }
+
+        // GET: Projects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HoursPMO/Controllers/WeekPerUserPerProjectsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoursPMO.DAL;
+using HoursPMO.Models;
+
+namespace HoursPMO.Controllers
+{
+    public class WeekPerUserPerProjectsController : Controller
+    {
+        private HoursContext db = new HoursContext();
+
+        // GET: WeekPerUserPerProjects
+        public ActionResult Index()
+        {
+            var weekPerUserPerProjects = db.WeekPerUserPerProjects.Include(w => w.Project).Include(w => w.User);
+            return View(weekPerUserPerProjects.ToList());
+        }
+
+        // GET: WeekPerUserPerProjects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WeekPerUserPerProject weekPerUserPerProject = db.WeekPerUserPerProjects.Find(id);
+            if (weekPerUserPerProject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(weekPerUserPerProject);
+        }
+
+        // GET: WeekPerUserPerProjects/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "ProjectName");
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName");
+            return View();
+        }
+
+        // POST: WeekPerUserPerProjects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WeekPerUserPerProjectID,FTO,Leaves,Possible,Actual,Billable,WorkingDays,WeekNo,Year,UserID,ProjectID,Created")] WeekPerUserPerProject weekPerUserPerProject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WeekPerUserPerProjects.Add(weekPerUserPerProject);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "ProjectName", weekPerUserPerProject.ProjectID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", weekPerUserPerProject.UserID);
+            return View(weekPerUserPerProject);
+        }
+
+        // GET: WeekPerUserPerProjects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WeekPerUserPerProject weekPerUserPerProject = db.WeekPerUserPerProjects.Find(id);
+            if (weekPerUserPerProject == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "ProjectName", weekPerUserPerProject.ProjectID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", weekPerUserPerProject.UserID);
+            return View(weekPerUserPerProject);
+        }
+
+        // POST: WeekPerUserPerProjects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WeekPerUserPerProjectID,FTO,Leaves,Possible,Actual,Billable,WorkingDays,WeekNo,Year,UserID,ProjectID,Created")] WeekPerUserPerProject weekPerUserPerProject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(weekPerUserPerProject).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ProjectID", "ProjectName", weekPerUserPerProject.ProjectID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", weekPerUserPerProject.UserID);
+            return View(weekPerUserPerProject);
+        }
+
+        // GET: WeekPerUserPerProjects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WeekPerUserPerProject weekPerUserPerProject = db.WeekPerUserPerProjects.Find(id);
+            if (weekPerUserPerProject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(weekPerUserPerProject);
+        }
+
+        // POST: WeekPerUserPerProjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WeekPerUserPerProject weekPerUserPerProject = db.WeekPerUserPerProjects.Find(id);
+            db.WeekPerUserPerProjects.Remove(weekPerUserPerProject);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
