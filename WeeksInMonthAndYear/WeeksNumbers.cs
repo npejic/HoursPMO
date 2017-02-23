5d79bcbce39a695809229bcbdec16e7d1c4c9e69
nik <npejic@gmail.com>
2017-02-23 11:53:50 +0100

    Added WeeksNumbers in repository

---
+++ WeeksInMonthAndYear/WeeksNumbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeksInMonthAndYear
+{
+    public class WeeksNumbers
+    {
+        public List<int> WeeksNumberInYearPerMonth(int SelectedMonthNo, int SelectedYearNo)
+        {
+            int weekCounter = 0;
+            //FirstMondayInYear initially get value of firtst day in year, whatever that day is
+            DateTime FirstMondayInYear = new DateTime(SelectedYearNo, 1, 1);
+
+            //while loop adds one day to FirstMondayInYear until FirstMondayInYear really is Monday
+            while (FirstMondayInYear.DayOfWeek != DayOfWeek.Monday)
+            {
+                FirstMondayInYear = FirstMondayInYear.AddDays(1);
+            }
+
+            List<DateTime> dates = new List<DateTime>();
+            //endDate represents last day in selected month and year, whatever that day is
+            DateTime endDate = new DateTime(SelectedYearNo, SelectedMonthNo, DateTime.DaysInMonth(SelectedYearNo, SelectedMonthNo));
+            
+            //dates get all Mondays from first Monday in year until last monday in selected month and year 
+            for (var dt = FirstMondayInYear; dt <= endDate; dt = dt.AddDays(7))
+            {
+                dates.Add(dt);
+            }
+
+            //weekCounter gets number of weeks from FirstMondayInYear to endDate
+            weekCounter = dates.Count();
+            
+            //week counts as 1 if number of working days is 3,4 or 5 days
+            if (FirstMondayInYear.DayOfWeek == DayOfWeek.Tuesday || FirstMondayInYear.DayOfWeek == DayOfWeek.Wednesday)
+                weekCounter++;
+            if (endDate.DayOfWeek == DayOfWeek.Monday || endDate.DayOfWeek == DayOfWeek.Tuesday)
+                weekCounter--;
+
+            //countOfWeeksInSelectedMonth are numbers of weeks in selected months and year
+            var countOfWeeksInSelectedMonth = dates.Count(a => a.Month == SelectedMonthNo);
+
+            //numbersOfWeeksInSelectedMonth is return value for this method, sorted reverse of countOfWeeksInSelectedMonth
+            
+            var numbersOfWeeksInSelectedMonth = new List<int>();
+            for (int i = countOfWeeksInSelectedMonth; i >= 0; i--)
+            {
+                numbersOfWeeksInSelectedMonth.Add(weekCounter - i);
+            }
+            
+            return numbersOfWeeksInSelectedMonth;
+        }
+    }
+}
